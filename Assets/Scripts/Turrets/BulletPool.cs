5631490b3a0c645f395e56f6b65666adb965f7de
Léonard Gombert <epoc4oftime@gmail.com>
2022-02-01 11:11:08 +0100

    Passing Bullet Pool ref on Turret config

--- Assets/Scripts/Turrets/BulletPool.cs
+++ Assets/Scripts/Turrets/BulletPool.cs
@@ -1,20 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletPool : MonoBehaviour
 {
     [SerializeField] private int _bulletPoolSize;
     [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private ShootingBehaviour[] _turrets;
 
     private ObjectPool<BulletBehaviour> _bulletPool;
 
-    public ObjectPool<BulletBehaviour> Pool => _bulletPool;
-
-    // Start is called before the first frame update
     void Awake()
     {
         _bulletPool = new ObjectPool<BulletBehaviour>(_bulletPoolSize, _bulletPrefab, transform);
         _bulletPool.FillPool();
+
+        foreach (var turret in _turrets)
+        {
+            turret.Config(_bulletPool);
+        }
     }
 }
--- Assets/Scripts/Turrets/ShootingBehaviour.cs
+++ Assets/Scripts/Turrets/ShootingBehaviour.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class ShootingBehaviour : MonoBehaviour
 {
-    [SerializeField] private BulletPool _bulletPool;
-    private GameObject _currBullet;
-
     [SerializeField, Tooltip("The number of bulets to fire per second.")] private float _rateOfFire;
     private float _timePassed;
+    private GameObject _currBullet;
 
-    [SerializeField] private Vector3 _barrelTip;    
+    private ObjectPool<BulletBehaviour> _bulletPool;
+
+    public void Config(ObjectPool<BulletBehaviour> bulletPool)
+    {
+        _bulletPool = bulletPool;
+    }
 
     private void Start()
     {
@@ -26,18 +29,15 @@ public class ShootingBehaviour : MonoBehaviour
         if (_timePassed >= _rateOfFire)
         {
             _timePassed = 0.0f;
-            Shoot();
+            FireTurrets();
         }
     }
 
-    private void Shoot()
+    private void FireTurrets()
     {
-        _currBullet = _bulletPool.Pool.GetFreeObject();
+        _currBullet = _bulletPool.GetFreeObject();
 
-        if (_currBullet)
-        {
-            _currBullet.transform.position = transform.position + _barrelTip;
-            _currBullet.transform.up = transform.up;
-        }
+        _currBullet.transform.position = transform.position;
+        _currBullet.transform.up = transform.up;
     }
 }
