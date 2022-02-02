94fd807e32da8decc59c4c0c932e31674c369c14
Léonard Gombert <epoc4oftime@gmail.com>
2022-02-02 16:18:09 +0100

    Small changes

--- Assets/Scripts/Base/PlayerBaseIntegrity.cs
+++ Assets/Scripts/Base/PlayerBaseIntegrity.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class PlayerBaseIntegrity : MonoBehaviour, IDamageable
 {
-    [SerializeField] private int _lifePoints;
+    [SerializeField] private int _maxLifePoints;
+    [SerializeField] private Slider _lifePointsSlider;
+    private int _currentLifePoints;
     [SerializeField] private UnityEvent _gameOver;
 
+    private void Start()
+    {
+        _currentLifePoints = _maxLifePoints;
+        SetSliderValue();
+    }
 
     public void Hit(GameObject otherObject)
     {
+        _currentLifePoints--;
+
+        SetSliderValue();
+
         otherObject.GetComponent<Pool_Object>().Deactivate();
 
-        _lifePoints--;
-
-        if (_lifePoints <= 0)
+        if (_currentLifePoints <= 0)
         {
             Destroy();
         }
@@ -22,5 +32,12 @@ public class PlayerBaseIntegrity : MonoBehaviour, IDamageable
     public void Destroy()
     {
         _gameOver.Invoke();
+    }
+
+    private void SetSliderValue()
+    {
+        float temp = _currentLifePoints / _maxLifePoints;
+        Debug.Log(temp  );
+        _lifePointsSlider.value = temp;
     }
 }
